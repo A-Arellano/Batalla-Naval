a3e61fe97d3cab92d3ae9dffb23395c433abc236
A-Arellano <krollarellano@gmail.com>
2017-09-07 17:08:27 -0300

    Se suman funciones de guardado y cargado. Tambien se cambio la manera de utilizar los barcos. Persiste el problema de la ubicacion del submarino

--- Batalla_Naval.cpp
+++ Batalla_Naval.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <windows.h>
 
 using namespace std;
 
@@ -9,100 +11,144 @@ struct Tablero
     int** columna;
 };
 
-struct Barco
+struct Pesquero
 {
     int taman;
-    int fila_i;
-    int columna_i;
-    int dir;
+    int fila_1;
+    int columna_1;
+};
+
+struct Submarino
+{
+    int taman;
+    int fila_1;
+    int fila_2;
+    int columna_1;
+    int columna_2;
+};
+
+struct Acorazado
+{
+    int taman;
+    int fila_1;
+    int fila_2;
+    int fila_3;
+    int columna_1;
+    int columna_2;
+    int columna_3;
+};
+
+struct Portaaviones
+{
+    int taman;
+    int fila_1;
+    int fila_2;
+    int fila_3;
+    int fila_4;
+    int columna_1;
+    int columna_2;
+    int columna_3;
+    int columna_4;
 };
 
 Tablero creartablero ();
 void mostrartablero (Tablero T_Naval);
-Barco crearbarco (int columna_i, int fila_i, int dir, int &cantbarc);
-void cargarbarcos (int &cantbarc, Barco &barco);
-void ubicarbarco (Barco barco, Tablero &tablero);
+void guardar_tablero (Tablero tableroguarda);
+Tablero cargar_tablero ();
+Pesquero crear_pesquero ();
+void ubicarsubmarino (Submarino &submarino, Tablero &tablero);
+Submarino crear_submarino (Tablero &tablero);
 
 int main()
 {
+    Pesquero p1;
+    Pesquero p2;
+    Submarino s1;
+    Submarino s2;
+    Acorazado a1;
+    Acorazado a2;
+    Portaaviones pa1;
+    Portaaviones pa2;
     int cantbarc1 = 1;
     int cantbarc2 = 1;
-    Barco pesquero1;
-    Barco submarino1;
-    Barco acorazado1;
-    Barco portaaviones1;
-    Barco pesquero2;
-    Barco submarino2;
-    Barco acorazado2;
-    Barco portaaviones2;
+    Submarino submarino;
+
     Tablero tablero = creartablero();
+     mostrartablero(tablero);
+    s1 = crear_submarino(tablero);
+    ubicarsubmarino(s1, tablero);
+    cout << submarino.fila_1 << ":" << submarino.columna_1 << "-" << submarino.fila_2 << ":" << submarino.columna_2 << endl;
     mostrartablero(tablero);
-    cargarbarcos(cantbarc1, portaaviones1);
-    ubicarbarco(portaaviones1,tablero);
-    mostrartablero(tablero);
-    cargarbarcos(cantbarc1, acorazado1);
-    ubicarbarco(acorazado1,tablero);
-    mostrartablero(tablero);
-    cargarbarcos(cantbarc1, submarino1);
-    ubicarbarco(submarino1,tablero);
-    mostrartablero(tablero);
-    cargarbarcos(cantbarc1, pesquero1);
-    ubicarbarco(pesquero1,tablero);
-    mostrartablero(tablero);
-
-
-
-
+    guardar_tablero(tablero);
     return 0;
 }
 
-Barco crearbarco (int columna_i, int fila_i, int dir, int &cantbarc)
+Pesquero crear_pesquero (Tablero &tablero)
 {
-    Barco barco;
-    barco.taman = cantbarc;
-    barco.columna_i = columna_i;
-    barco.fila_i = fila_i;
-    barco.dir = dir;
-    cantbarc++;
-    return barco;
+    Pesquero pesquero;
+    pesquero.taman = 1;
+    cin >> pesquero.columna_1;
+    cin >> pesquero.fila_1;
+    return pesquero;
 }
 
-void cargarbarcos (int &cantbarc, Barco &barco)
+Submarino crear_submarino (Tablero &tablero)
 {
-    int columna;
-    int fila;
+
+    int direccion;
+    Submarino submarino;
+    submarino.taman = 2;
+    cout << "Columna inicial submarino: ";
+    cin >> submarino.columna_1;
+    cout << endl << "Fila incial submarino: ";
+    cin >> submarino.fila_1;
+    return submarino;
+}
+
+void ubicarsubmarino (Submarino &submarino, Tablero &tablero)
+{
     int dir;
-    cout << "Fila del barco N " << cantbarc << endl;
-    cin >> columna;  // Creando Portaaviones
-    cout << "Columna del barco N " << cantbarc << endl;
-    cin >> fila;
-    cout << "Direccion del barco N " << cantbarc << endl;
+    cout << "cin >> dir" << endl;
     cin >> dir;
-    barco = crearbarco(columna, fila, dir, cantbarc);
+    if (dir == 1){ // Derecha
+        for (int i = submarino.columna_1 ; i < submarino.columna_1+submarino.taman ; i++){
+            tablero.columna[submarino.fila_1][i] = submarino.taman;
+            submarino.fila_2 = submarino.fila_1;
+            submarino.columna_2 = i;
+        }
+    }
+    else if (dir == 2){  // Arriba
+        for (int i = submarino.fila_1 ; i > submarino.fila_1-submarino.taman ; i--){
+            tablero.columna[i][submarino.columna_1] = submarino.taman;
+            submarino.columna_2 = submarino.columna_1;
+            submarino.fila_2 = i;
+        }
+    }
+    else if (dir == 3){ // Izquierda
+        for (int i = submarino.columna_1 ; i > submarino.columna_1-submarino.taman ; i--){
+            tablero.columna[submarino.fila_1][i] = submarino.taman;
+            submarino.fila_2 = submarino.fila_1;
+            submarino.columna_2 = i;
+        }
+    }
+    else if (dir == 4){ // Abajo
+        for (int i = submarino.fila_1 ; i < submarino.fila_1+submarino.taman ; i++){
+            tablero.columna[i][submarino.columna_1] = submarino.taman;
+            submarino.columna_2 = submarino.columna_1;
+            submarino.fila_2 = i;
+        }
+    }
 }
 
-void ubicarbarco (Barco barco, Tablero &tablero)
+Acorazado crear_acorazado (Tablero &tablero)
 {
-    if (barco.dir == 1){
-        for (int i = barco.columna_i ; i < barco.columna_i+barco.taman ; i++){
-            tablero.columna[i][barco.fila_i] = barco.taman;
-        }
-    }
-    else if (barco.dir == 2){
-        for (int i = barco.fila_i ; i > barco.fila_i-barco.taman ; i--){
-            tablero.columna[barco.columna_i][i] = barco.taman;
-        }
-    }
-    else if (barco.dir == 3){
-        for (int i = barco.columna_i ; i > barco.columna_i-barco.taman ; i--){
-            tablero.columna[i][barco.fila_i] = barco.taman;
-        }
-    }
-    else if (barco.dir == 4){
-        for (int i = barco.fila_i ; i < barco.fila_i+barco.taman ; i++){
-            tablero.columna[barco.columna_i][i] = barco.taman;
-        }
-    }
+    Acorazado acorazado;
+    acorazado.taman = 3;
+
+    cin >> acorazado.columna_1;
+    cin >> acorazado.fila_1;
+    cin >> acorazado.columna_2;
+    cin >> acorazado.fila_2;
 }
 
 Tablero creartablero ()
@@ -146,4 +192,44 @@ void mostrartablero (Tablero T_Naval)
         cout << endl;
     }
 }
+
+Tablero cargar_tablero ()
+{
+    ifstream miTablero;
+    Tablero tablerocarga;
+    tablerocarga = creartablero();
+    miTablero.open("tablerete.txt");
+        if(miTablero.is_open())
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+
+                   miTablero >> tablerocarga.columna[i][j];
+                }
+            }
+        }
+    return tablerocarga;
+}
+
+void guardar_tablero (Tablero tableroguarda)
+{
+    ofstream miTablero;
+    miTablero.open("tablerete.txt");
+        if(miTablero.is_open())
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    miTablero << tableroguarda.columna[i][j];
+                    miTablero << ' ';
+                }
+            }
+        }
+        cout << "Se ha guardado el barco con exito!" << endl;
+        system("pause");
+}
+
 
